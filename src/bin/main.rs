535df0bbca19c546de932048423697dd7b02904b
gardrek <gardrek.the.destroyer@gmail.com>
2018-05-25 04:21:50 -0500

    added features in progress

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,5 @@
 extern crate stvm;
-use stvm::STVM;
+use stvm::{STVM, Lang};
 
 fn main() -> () {
     let zero = 0u8;
@@ -8,13 +8,17 @@ fn main() -> () {
     assert_eq!(-(std::i8::MIN + 1), std::i8::MAX);
     assert_eq!(-(std::isize::MIN + 1), std::isize::MAX);
 
-    //let mut main_vm = STVM::new();
+    let mut main_vm = STVM::from_file("LostKingdomBF/LostKng.b");
     //let mut main_vm = STVM::from_file("tictactoe.bf");
     //let mut main_vm = STVM::from_file("life.bf");
     //let mut main_vm = STVM::from_file("hanoi.bf");
-    //let mut main_vm = STVM::from_code(">");
-    let mut main_vm = STVM::from_code("[]+++++>++<[->>+<<]>>>+++[-]++[<]");
-    //let mut main_vm = STVM::from_code("+++++[>+++<-]>");
+
+    let mut simpletest = STVM::from_code(Lang::BF, "[]+++++>++<[->>+<<]>>>+++[-]++[<]");
+    let mut multiply = STVM::from_code(Lang::BF, "+++++[>+++<-]>");
+
+    simpletest.run();
+    multiply.run();
+
     //println!("{}", main_vm.read());
 
     /*let mut main_vm = STVM::from_code(
@@ -37,6 +41,6 @@ fn main() -> () {
     //println!("{:?}\n--------\n", main_vm);
 
     main_vm.run();
-    main_vm.debug_print();
-    println!("\n\n--------\n{:?}", main_vm);
+    //main_vm.debug_print();
+    //println!("\n\n--------\n{:?}", main_vm);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,36 @@
+/*!
+ * Stack-Tape Virtual Machine
+ */
+
 use std::io::{self, Read, Write};
 use std::ops::{Index, IndexMut};
 
+/// Supported languages for compiling
 #[derive(Debug)]
 pub enum Lang {
     BF,
+    LISP,
 }
 
+/// A program's source code and compiled bytecode
 #[derive(Debug)]
 pub struct Program {
     lang: Lang,
     sourcecode: String,
     //tokenlist, ast, etc?
-    //commandlist: Tape<Command>,
+    commandlist: Tape<Command>,
     //bytecode: Tape<i8>,
 }
 
 #[derive(Debug)]
-enum Flag {
-    Zero,
-    Overflow,
+struct Flags {
+    zero: bool,
+    overflow: bool,
+}
+
+#[derive(Debug)]
+struct Registerset {
+    flags: Flags,
 }
 
 #[derive(Debug, Clone)]
@@ -30,10 +42,10 @@ struct Tape<T: Copy> {
 #[derive(Debug)]
 pub struct STVM {
     program: Program,
-    sourcecode: String,
     tape: Tape<i8>,
     bytecode: Tape<Command>,
     stack: Tape<i8>,
+    registerset: Registerset
     //input: Vec<i8>, // should be a FIFO tho
     //output: Vec<i8>,
 }
@@ -61,12 +73,27 @@ enum Command {
     Pop,
 }
 
+impl Registerset {
+    pub fn new() -> Self {
+        Self {
+            flags: Flags {
+                zero: false,
+                overflow: false,
+            },
+        }
+    }
+}
+
 impl Program {
     pub fn new(lang: Lang, sourcecode: &str) -> Self {
-        Self {lang, sourcecode: sourcecode.to_string()}
+        Self {
+            lang,
+            sourcecode: sourcecode.to_string(),
+            commandlist: Tape { data: vec![], cursor: 0 },
+        }
     }
 
-    pub fn from_file(filename: String) -> Self {
+    pub fn from_file(filename: &str) -> Self {
         // FIXME: choose source language from file name extension
         let lang = Lang::BF;
 
@@ -76,6 +103,158 @@ impl Program {
         f.read_to_string(&mut sourcecode).expect("something went wrong reading the file");
 
         Self::new(lang, &sourcecode)
+    }
+
+    fn compile_bf(&mut self) {
+        //if self.lang != Lang::BF {panic!("")};
+        match self.lang {
+            Lang::BF => (),
+            _ => panic!("tried to compile wrong language"),
+        }
+
+        use self::Command::*;
+        use std::collections::HashMap;
+
+        let mut ops = HashMap::new();
+        ops.insert('+', Inc);
+        ops.insert('-', Dec);
+        ops.insert('>', IncTape);
+        ops.insert('<', DecTape);
+        ops.insert('.', OutputByte);
+        ops.insert(',', InputByte);
+        ops.insert('[', StartLoop);
+        ops.insert(']', EndLoop);
+
+        for c in self.sourcecode.chars() {
+            if let Some(&com) = ops.get(&c) {
+                self.commandlist.push(com);
+            }
+        }
+        self.commandlist.push(HaltAlways);
+
+        self.optim_bf();
+
+        let mut stack = vec![];
+        for index in 0..self.commandlist.len() {
+            let com = self.commandlist[index];
+            match com {
+                StartLoop => {
+                    stack.push(index);
+                }
+                EndLoop => {
+                    let f = match stack.pop() {
+                        None => panic!("Unmatched bracket!"),
+                        Some(x) => x,
+                    };
+                    self.commandlist[index] = JumpIfNonzero(f);
+                    self.commandlist[f] = JumpIfZero(index);
+                }
+                _ => {
+                    //
+                }
+            };
+        }
+        if stack.len() != 0 {
+            panic!("unmatched bracket!")
+        };
+    }
+
+    fn optim_bf(&mut self) {
+        /*
+        use self::Command::*;
+
+        println!("Optimizing");
+        //println!("{:?}", self.bytecode);
+        let old_length = self.bytecode.len();
+        if self.bytecode.len() == 0 {
+            println!("command list is empty, skipping optimize");
+            return;
+        };
+        println!("length: {} commands\n", self.bytecode.len());
+
+        let mut old = Tape { data: vec![], cursor: 0 };
+
+        std::mem::swap(&mut old, &mut self.bytecode);
+        let mut count:isize;
+        let mut index = 0;
+        while index < old.len() {
+            let current = old[index];
+            match current {
+                Inc | Dec | IncTape | DecTape => {
+                    count = 1;
+                    while index < old.len() - 1 // next is within bounds
+                        && old[index + 1] == current // same commannd
+                        //count < 127 // less than i8 max, as Add takes an i8
+                        // TODO: since MoveTape takes an isize, perhaps it
+                        // would make sense to support more than ±127 for it
+                    {
+                        match current {
+                            Inc | Dec => if count >= std::i8::MAX as isize {break},
+                            IncTape | DecTape => if count >= std::isize::MAX {break},
+                            _ => panic!(),
+                        };
+                        count += 1;
+                        index += 1;
+                    }
+                    match current {
+                        Inc => self.bytecode.push(Add(count as i8)),
+                        Dec => self.bytecode.push(Add(-count as i8)),
+                        IncTape => self.bytecode.push(MoveTape(count)),
+                        DecTape => self.bytecode.push(MoveTape(-count)),
+                        _ => panic!(),
+                    };
+                },
+                StartLoop => {
+                    if index < old.len() - 1 && old[index + 1] == EndLoop {
+                        index += 1;
+                        self.bytecode.push(HaltIfNonzero)
+                    } else {
+                        self.bytecode.push(current)
+                    }
+                },
+                _ => self.bytecode.push(current),
+            };
+            index += 1;
+        }
+
+        println!("First pass");
+        //println!("{:?}", self.bytecode);
+        println!("length after first pass: {} commands", self.bytecode.len());
+        println!("ratio: {}%\n", (self.bytecode.len() * 100) / old_length);
+
+        old = Tape { data: vec![], cursor: 0 };
+        std::mem::swap(&mut old, &mut self.bytecode);
+
+        let mut index = 0;
+        while index < old.len() {
+            if index < old.len() - 2 {
+                match &old.data[index..index + 3] {
+                &[StartLoop, middle, EndLoop] => {
+                    match middle {
+                        Add(1) | Add(-1) => {
+                            self.bytecode.push(Set(0));
+                            index += 2;
+                        },
+                        MoveTape(x) => {
+                            self.bytecode.push(Seek(x));
+                            index += 2;
+                        },
+                        _ => self.bytecode.push(old[index]),
+                    };
+                },
+                  _ => self.bytecode.push(old[index]),
+                }
+            } else {
+                self.bytecode.push(old[index]);
+            }
+            index += 1;
+        }
+
+        println!("Second Pass");
+        //println!("{:?}", self.bytecode);
+        println!("length after second pass: {} commands", self.bytecode.len());
+        println!("ratio: {}%\n", (self.bytecode.len() * 100) / old_length);
+        */
     }
 }
 
@@ -114,7 +293,7 @@ impl<T: Copy> Tape<T> {
         self[index] = value;
     }
 
-    fn len(&mut self) -> usize {
+    fn len(&self) -> usize {
         self.data.len()
     }
 
@@ -174,37 +353,39 @@ impl STVM {
     pub fn new() -> Self {
         Self {
             program: Program {
-                sourcecode: String::from(""),
                 lang: Lang::BF,
-                //commandlist: Tape { data: vec![], cursor: 0 },
+                sourcecode: String::from(""),
+                commandlist: Tape { data: vec![], cursor: 0 },
                 //bytecode: Tape { data: vec![], cursor: 0 },
             },
             tape: Tape { data: vec![0], cursor: 0 },
             stack: Tape { data: vec![0], cursor: 0 },
-            sourcecode: String::from(""),
             bytecode: Tape { data: vec![], cursor: 0 },
+            registerset: Registerset::new(),
         }
     }
 
-    pub fn from_code(code: &str) -> Self {
+    fn set_program(&mut self, program: Program) {
+        self.program = program;
+    }
+
+    pub fn from_code(lang: Lang, sourcecode: &str) -> Self {
         let mut vm = Self::new();
-        vm.compile(code);
+        vm.set_program(Program::new(lang, sourcecode));
+        vm.compile();
         vm
     }
 
     pub fn from_file(filename: &str) -> Self {
-        use std::fs::File;
-        let mut f = File::open(filename).expect("file not found");
-        let mut contents = String::new();
-        f.read_to_string(&mut contents).expect("something went wrong reading the file");
-
-        Self::from_code(&contents)
+        let mut vm = Self::new();
+        vm.set_program(Program::from_file(filename));
+        vm.compile();
+        vm
     }
 
-    pub fn compile(&mut self, code: &str) {
+    pub fn compile(&mut self) {
         use self::Command::*;
         use std::collections::HashMap;
-        self.sourcecode = String::from(code);
 
         let mut ops = HashMap::new();
         ops.insert('+', Inc);
@@ -216,7 +397,7 @@ impl STVM {
         ops.insert('[', StartLoop);
         ops.insert(']', EndLoop);
 
-        for c in self.sourcecode.chars() {
+        for c in self.program.sourcecode.chars() {
             if let Some(&com) = ops.get(&c) {
                 self.bytecode.push(com);
             }
@@ -265,25 +446,32 @@ impl STVM {
         let mut old = Tape { data: vec![], cursor: 0 };
 
         std::mem::swap(&mut old, &mut self.bytecode);
-        let mut count;
+        let mut count:isize;
         let mut index = 0;
         while index < old.len() {
             let current = old[index];
             match current {
                 Inc | Dec | IncTape | DecTape => {
                     count = 1;
-                    while index < old.len() - 1 && // next is within bounds
-                        old[index + 1] == current && // same commannd
-                        count < 127 // less than an i8, as Add takes an i8
+                    while index < old.len() - 1 // next is within bounds
+                        && old[index + 1] == current // same commannd
+                        //count < 127 // less than i8 max, as Add takes an i8
+                        // TODO: since MoveTape takes an isize, perhaps it
+                        // would make sense to support more than ±127 for it
                     {
+                        match current {
+                            Inc | Dec => if count >= std::i8::MAX as isize {break},
+                            IncTape | DecTape => if count >= std::isize::MAX {break},
+                            _ => panic!(),
+                        };
                         count += 1;
                         index += 1;
                     }
                     match current {
                         Inc => self.bytecode.push(Add(count as i8)),
                         Dec => self.bytecode.push(Add(-count as i8)),
-                        IncTape => self.bytecode.push(MoveTape(count as isize)),
-                        DecTape => self.bytecode.push(MoveTape(-(count as isize))),
+                        IncTape => self.bytecode.push(MoveTape(count)),
+                        DecTape => self.bytecode.push(MoveTape(-count)),
                         _ => panic!(),
                     };
                 },
@@ -351,7 +539,7 @@ impl STVM {
 
             let com = self.bytecode.read();
             match com {
-                Add(n) => if self.tape.add(n) {self.set_flag(Flag::Overflow)},
+                Add(n) => if self.tape.add(n) {self.set_overflow_flag()},
                 Set(n) => self.tape.write(n),
                 MoveTape(n) => self.tape.move_cursor(n),
                 Seek(n) => while self.read() != 0 { // TODO: Optimize?
@@ -373,6 +561,9 @@ impl STVM {
                         Err(e) => panic!(e),
                         Ok(n) => if n == 1 {
                             self.tape.write(buffer[0] as i8);
+                        } else if n == 0 {
+                            halt = true;
+                            println!("no bytes read from input, halting")
                         } else {
                             panic!("wrong number of bytes read! {} bytes", n)
                         },
@@ -413,16 +604,16 @@ impl STVM {
 
     pub fn each_cell(&self) -> std::slice::Iter<'_, i8> { self.tape.iter() }
 
-    pub fn debug_print(&self) -> () {
-        println!("{:?}", self.tape);
+    pub fn debug_print(&self) {
+        println!("{:?}", self.tape.len());
     }
 
-    fn set_flag(&mut self, flag: Flag) -> () {
-        println!("Flag {:?} set!", flag)
+    fn set_overflow_flag(&mut self) -> () {
+        //println!("overflow flag set!")
     }
 
-    fn clear_flag(&mut self, flag: Flag) -> () {
-        println!("Flag {:?} cleared!", flag)
+    fn clear_overflow_flag(&mut self) -> () {
+        //println!("overflow flag cleared!")
     }
 }
 
@@ -431,7 +622,7 @@ mod tests {
     #[test]
     fn it_works() {
         let mut test_vm = super::STVM::new();
-        test_vm.compile("+++++[>+++<-]>");
+        test_vm.set_program(Program {lang: Lang::bf, sourecode: "+++++[>+++<-]>"});
         while test_vm.step() {};
         assert_eq!(test_vm.read(), 15);
     }
